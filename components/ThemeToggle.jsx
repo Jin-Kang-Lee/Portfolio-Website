4a84c94513fcbb9a50d531a99dcb78d8a673033e
Jin_Kang <138515875+Jin-Kang-Lee@users.noreply.github.com>
2024-07-04 20:19:53 +0800

    Update ThemeToggle.jsx

--- components/ThemeToggle.jsx
+++ components/ThemeToggle.jsx
@@ -5,11 +5,11 @@ import { FaMoon } from 'react-icons/fa';
 import { BsSunFill } from 'react-icons/bs';
 
 const ThemeToggle = () => {
-    const [darkMode, setDarkMode] = useState(true)
+    const [darkMode, setDarkMode] = useState(false)
 
     useEffect(() => {
         const theme = localStorage.getItem("theme")
-        if (theme === "dark") setDarkMode(true)
+        if (theme === "dark") setDarkMode(false)
     }, [])
 
     useEffect(() => {
