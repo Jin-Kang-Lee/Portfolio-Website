45a071ae8f4b7ba204804a209deefd12e40b42f8
Jin_Kang <138515875+Jin-Kang-Lee@users.noreply.github.com>
2024-07-05 17:24:45 +0800

    Update Experience.jsx

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -8,20 +8,20 @@ const Experience = () => {
             <p className='text-xl tracking-widest uppercase text-[#504be7] dark:text-[#FF2E63]'>Experience</p>
             <h2 className='py-4'>Where have I been</h2>
             <div className=' gap-8 '>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 bg-slate-200'>
                     <div className='gap-4 justify-center items-center'>
                         <div className='m-auto'>
                             <Image src='/assets/experience/pwc_logo.png' 
                             width={64} 
                             height={64} 
                             alt='/'/>
-                            <p>PricewaterhouseCoopers</p>
+                            <p className='text-black'>PricewaterhouseCoopers</p>
                         </div>
-                        <div className='py-4 text-gray-600 dark:bg-dark dark:text-[#EAEAEA]'>
+                        <div className='py-4 text-gray-600 '>
                             <p>Risk Assurance - Cloud Trust Internship</p>
                             <p>October 2021 - March 2022 (6 Months)</p>
                         </div>
-                        <div className=' text-gray-600 dark:bg-dark dark:text-[#EAEAEA]'>
+                        <div className=' text-gray-600 '>
                         <p>- Supported the team in conducting a comprehensive controls review of the client's cloud environment.</p>
                         <p>- Contributed to the development of content and materials for a cloud security webinar tailored for client engagement.</p>
                         <p>- Collaborated with the team to formulate a proposal outlining cloud advisory services for a transformative project.</p>
@@ -31,20 +31,20 @@ const Experience = () => {
                 <div className='py-6'>
 
                 </div>
-                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+                <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300 bg-slate-200'>
                     <div className='gap-4 justify-center items-center'>
                         <div className='m-auto'>
                             <Image src='/assets/experience/China-Uni-Logo.png' 
                             width={64} 
                             height={64} 
                             alt='/'/>
-                            <p>Suzhou University of Science and Technology</p>
+                            <p className='text-black'>Suzhou University of Science and Technology</p>
                         </div>
-                        <div className='py-4 text-gray-600 dark:bg-dark dark:text-[#EAEAEA]'>
+                        <div className='py-4 text-gray-600 '>
                             <p>Data Analyst Internship</p>
                             <p>September 2021 - October 2021 (2 Months)</p>
                         </div>
-                        <div className=' text-gray-600 dark:bg-dark dark:text-[#EAEAEA]'>
+                        <div className=' text-gray-600 '>
                         <p>- Collaborated with the team and professor to address the annual Data Analytics challenge proposed by professionals in the Chinese visualization industry</p>
                         <p>- Executed tasks encompassing Data Mining, Data Cleaning, Text Analysis, Data Visualization, and Storytelling</p>
                         </div>
